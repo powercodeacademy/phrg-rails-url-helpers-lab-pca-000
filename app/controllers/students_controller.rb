bea2160f0bcc83dafcfeabc63bd4e219f4b7552d
Stephen McKeon <stephen.mckeon@powerhrg.com>
2025-08-15 09:51:24 -0400

    Toggle active for student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,9 +1,20 @@
 class StudentsController < ApplicationController
+  before_action :set_student, only: [:show, :activate]
+
   def index
     @students = Student.all
   end
 
-  def show
+  def show; end
+
+  def activate
+    @student.toggle!(:active)
+    redirect_to student_path(@student)
+  end
+
+private
+
+  def set_student
     @student = Student.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :students, only: [:index, :show]
+  resources(:students, only: [:index, :show])
+
+  get "students/:id/activate", to: "students#activate", as: "activate_student"
 end
